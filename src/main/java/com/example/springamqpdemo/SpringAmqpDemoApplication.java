b883393622c76dcd0b0b34aaf510f347a1ab6429
此昵称不存在 <878740953@qq.com>
2023-02-24 22:15:33 +0800

    SpringAMQPDemo环境初始化

---
+++ src/main/java/com/example/springamqpdemo/SpringAmqpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springamqpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAmqpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAmqpDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springamqpdemo/SpringAmqpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springamqpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAmqpDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
